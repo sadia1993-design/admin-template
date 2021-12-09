c975102eaf97a9288b6dac1a171c3dca2c2628c6
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2021-12-09 20:35:47 +0600

    starter

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+
+
+<footer class="main-footer">
+  <strong>Copyright &copy; 2021 <a href="#">IDB-BISEW, BITL WDPF ROUND-46</a>.</strong>
+  All rights reserved.
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 1.0.0
+  </div>
+</footer>
+
+
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+  integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+  integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.1/datatables.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/js/adminlte.js"></script>
+</body>
+
+</html>
---
+++ sidebar.php
@@ -0,0 +1,60 @@
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="" class="brand-link text-center text-lg">
+      <span class="brand-text font-weight-bold" style="font-size: 25px ; display:block">DASHBOARD</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+
+        <div class="info">
+
+          <a href="#" class="d-block">
+            Welcome,
+          </a>
+
+        </div>
+      </div>
+
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+          <li class="nav-item">
+            <a href="dashboard.php" class="nav-link ">
+              <i class="fas fa-tachometer-alt"> </i>
+              <p> Dashboard
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link ">
+              <i class="fas fa-sitemap"></i>
+              <p> Department
+                <i class="fas fa-angle-left right"> </i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="department.php" class="nav-link">
+                  <i class="fas fa-sitemap"></i>
+                  <p>Add Department</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+
+
+
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
